b119bc2e16c47eee61977831c0104fcd0b538b5c
ymentha14 <yann.mentha@gmail.com>
2023-12-27 12:25:52 +1100

    Refactor: remove disclaimer

--- pydantic_ssm_settings/settings.py
+++ pydantic_ssm_settings/settings.py
@@ -14,11 +14,11 @@ from .source import AwsSsmSettingsSource
 logger = logging.getLogger(__name__)
 
 
-
 class BaseSettingsV2(BaseSettings):
     def __init__(self, *args, ssm_prefix: str = "/", **kwargs: Any) -> None:
         self.__dict__["__ssm_prefix"] = ssm_prefix
         super().__init__(self, *args, **kwargs)
+
 
 class AwsSsmSourceConfig(BaseSettingsV2):
     def settings_customise_sources(
@@ -31,18 +31,13 @@ class AwsSsmSourceConfig(BaseSettingsV2):
     ) -> Tuple[PydanticBaseSettingsSource, ...]:
         ssm_settings = AwsSsmSettingsSource(
             settings_cls=settings_cls,
-            ssm_prefix=self.__dict__["__ssm_prefix"] ,
+            ssm_prefix=self.__dict__["__ssm_prefix"],
         )
 
         return (
             init_settings,
             env_settings,
             dotenv_settings,
-            # Usurping the `secrets_dir` arg. We can't expect any other args to
-            # be passed to # the Settings module because Pydantic will complain
-            # about unexpected arguments. `secrets_dir` comes from `_secrets_dir`,
-            # one of the few special kwargs that Pydantic will allow:
-            # https://github.com/samuelcolvin/pydantic/blob/45db4ad3aa558879824a91dd3b011d0449eb2977/pydantic/env_settings.py#L33
             ssm_settings,
-            file_secret_settings
+            file_secret_settings,
         )
