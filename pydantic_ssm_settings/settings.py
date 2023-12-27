39835025bc15d098d5ae6e401e6daf956a776eca
ymentha14 <yann.mentha@gmail.com>
2023-12-27 17:20:57 +1100

    Doc: add docs and rename BaseClass

--- pydantic_ssm_settings/settings.py
+++ pydantic_ssm_settings/settings.py
@@ -18,16 +18,23 @@ logger = logging.getLogger(__name__)
 class SsmSettingsConfigDict(SettingsConfigDict):
     ssm_prefix : str
 
-class BaseSettingsV2(BaseSettings):
+class BaseSettingsSsmWrapper(BaseSettings):
+    """
+    Wrapper to store the _ssm_prefix parameter as an instanc attribute.
+    Need a direct access to the attributes dictionary to avoid raising a pydantic exception
+    """
     def __init__(self, *args, _ssm_prefix: str = None, **kwargs: Any) -> None:
         """
-        Need a direct access to the attributes dictionary to avoid a pydantic exception
+        Args:
+            _ssm_prefix: Prefix for all ssm parameters. Must be an absolute path, separated by "/". NB:
+            unlike its _env_prefix counterpart, _ssm_prefix is treated case sensitively regardless of the
+            _case_sensitive parameter value. 
         """
         self.__dict__["__ssm_prefix"] = _ssm_prefix
         super().__init__(self, *args, **kwargs)
 
 
-class AwsSsmSourceConfig(BaseSettingsV2):
+class AwsSsmSourceConfig(BaseSettingsSsmWrapper):
     def settings_customise_sources(
         self,
         settings_cls: Type[BaseSettings],
